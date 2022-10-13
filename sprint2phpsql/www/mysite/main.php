7daa28213c72819df604a9cf2769cf7c8233f4ce
sandra <speteiror@fpcoruna.afundacion.org>
2022-10-13 12:23:52 +0200

    borde tabla

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -10,19 +10,23 @@
 	<?php
 	$query = 'SELECT * FROM tPeliculas';
 	$result = mysqli_query($db, $query) or die('Query error');
+	echo '<table style="border:1px solid black;">';
 	while ($row = mysqli_fetch_array($result)){
-		echo $row['0'];
-		echo '<br>';
-		echo $row['1'];
-		echo '<br>';
-		echo '<img src="'.$row['2'];
-		echo '"width="80" height="120">';
-		echo '<br>';
-		echo $row['3'];
-		echo '<br>';
-		echo $row['4'];
-		echo '<br>';
+	  echo '<tr>';
+		echo '<td>';
+		  echo $row['0'];
+		echo '</td>';
+		echo '<td>';
+			echo '<a href="/detail.php?id='.$row['0'].'">'.$row['1'].'</a>';
+		echo '</td>';
+		echo '<td>';
+		  echo '<img src="'.$row['2'];
+		  echo '"width="80" height="120">';
+		echo '</td>';
+	  echo '</tr>';
 	}
+	echo '</table>';
+	mysqli_close($db);
 	?>
 </body>
 </html>
