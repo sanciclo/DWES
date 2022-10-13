31a3dfddf5e5db6db0cac393aafa96e7a4d13b43
sandra <speteiror@fpcoruna.afundacion.org>
2022-10-13 11:14:19 +0200

    actualización ejercicio2

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -6,6 +6,23 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión establecida</h1>
+	<h1>Conexión establecida</h1>
+	<?php
+	$query = 'SELECT * FROM tPeliculas';
+	$result = mysqli_query($db, $query) or die('Query error');
+	while ($row = mysqli_fetch_array($result)){
+		echo $row['0'];
+		echo '<br>';
+		echo $row['1'];
+		echo '<br>';
+		echo '<img src="'.$row['2'];
+		echo '">';
+		echo '<br>';
+		echo $row['3'];
+		echo '<br>';
+		echo $row['4'];
+		echo '<br>';
+	}
+	?>
 </body>
 </html>
