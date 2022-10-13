b8fc5b4fe71fe49a1ebe1a07ec2812e940f1ef46
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-13 10:53:38 +0200

    Subido .sql

---
+++ sprint2phpsql/urlpeliculas.sql
@@ -0,0 +1,10 @@
+use database mysitedb;
+
+UPDATE tPeliculas SET url_imagen= "https://i.ebayimg.com/images/g/DLkAAOSwdLJfcflD/s-l1600.jpg" where id=1;
+
+UPDATE tPeliculas SET url_imagen= "https://filmandes.com/wp/wp-content/uploads/2021/08/WhatsApp-Image-2021-08-12-at-23.09.09.jpeg" where id=2;
+
+UPDATE tPeliculas SET url_imagen= "https://as01.epimg.net/meristation/imagenes/2022/09/30/reportajes/1664534991_626157_1664615989_noticia_normal.jpg" where id=3;
+
+UPDATE tPeliculas SET url_imagen= "https://m.media-amazon.com/images/I/51Tdim4puhL.jpg" where id=4;
+
