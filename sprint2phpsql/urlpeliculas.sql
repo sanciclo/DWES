e9fe7d7b52de1f6b701d6c9b5b9588b533be06ab
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-13 11:37:19 +0200

    Add files via upload

--- sprint2phpsql/urlpeliculas.sql
+++ sprint2phpsql/urlpeliculas.sql
@@ -2,7 +2,7 @@ use mysitedb;
 
 UPDATE tPeliculas SET url_imagen= "https://i.ebayimg.com/images/g/DLkAAOSwdLJfcflD/s-l1600.jpg" where id=1;
 
-UPDATE tPeliculas SET url_imagen= "https://filmandes.com/wp/wp-content/uploads/2021/08/WhatsApp-Image-2021-08-12-at-23.09.09.jpeg" where id=2;
+UPDATE tPeliculas SET url_imagen= "https://m.media-amazon.com/images/I/41lAzkJW+2L._SX331_BO1,204,203,200_.jpg" where id=2;
 
 UPDATE tPeliculas SET url_imagen= "https://as01.epimg.net/meristation/imagenes/2022/09/30/reportajes/1664534991_626157_1664615989_noticia_normal.jpg" where id=3;
 
