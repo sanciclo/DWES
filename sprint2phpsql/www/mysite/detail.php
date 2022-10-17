98e2a1d0e475f902d1ae4f7f7dc7fb4d3ca86afd
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-17 13:08:27 +0200

    detail.php con formulario

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -9,7 +9,7 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
         die('No se ha especificado una película');
     }
     $pelicula_id = $_GET['id'];
-    $query = 'SELECT * FROM tPeliculas WHERE id=' . $pelicula_id;
+    $query = 'SELECT * FROM tpelicula WHERE id=' . $pelicula_id;
     $result = mysqli_query($db, $query) or die('Query error');
     $only_row = mysqli_fetch_array($result);
     echo '<h1>' . $only_row['nombre'] . '</h1>';
@@ -19,7 +19,7 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
     <h3>Comentarios:</h3>
     <ul>
         <?php
-        $query2 = 'SELECT * FROM tComentarios WHERE pelicula_id=' . $pelicula_id;
+        $query2 = 'SELECT * FROM tcomentario WHERE pelicula_id=' . $pelicula_id;
         $result2 = mysqli_query($db, $query2) or die('Query error');
         while ($row = mysqli_fetch_array($result2)) {
             echo '<li>' . $row['comentario'] . '</li>';
@@ -27,6 +27,12 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
         mysqli_close($db);
         ?>
     </ul>
+    <p>Deja un nuevo comentario:</p>
+        <form action="/comment.php" method="post">
+        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
+        <input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+        <input type="submit" value="Comentar">
+        </form>
 </body>
 
 </html>
