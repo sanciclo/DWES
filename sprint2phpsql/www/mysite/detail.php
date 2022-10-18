177ee86bf6d26b80331517be0fc837cb927f1e0d
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-18 14:28:22 +0200

    Add files via upload

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -20,9 +20,14 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
     <ul>
         <?php
         $query2 = 'SELECT * FROM tComentarios WHERE pelicula_id=' . $pelicula_id;
-        $result2 = mysqli_query($db, $query2) or die('Query error');
+        $result2 = mysqli_query($db, $query2) or die('Query error2');
         while ($row = mysqli_fetch_array($result2)) {
-            echo '<li>' . $row['comentario'] . '</li>';
+            $fecha = $row['fecha'];
+					if (is_null($fecha)) {
+						echo '<li>'.$row['1'].' (Sin fecha registrada)</li>';
+					}else {
+						echo '<li>'.$row['1'].' ('.$row['fecha'].')</li>';
+					}
         }
         mysqli_close($db);
         ?>
@@ -32,6 +37,7 @@ $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
         <textarea rows="4" cols="50" name="new_comment"></textarea><br>
         <input type="hidden" name="id" value="<?php echo $pelicula_id; ?>">
         <input type="submit" value="Comentar">
+        <input type="hidden" name="fecha" value="<?php echo $fecha; ?>">
     </form>
 </body>
 
