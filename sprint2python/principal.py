434fb17c087c758364fa9fab89c0c4cc510cac22
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-19 14:49:08 +0200

    Ejercicio 5 python

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,7 +1,23 @@
+from factorial import func_factorial
 from factorial2 import func_factorial2
 
-num = int(input("Introduzca un número mayor que 0: "))
-if num < 0:
-    print("Número negativo. Introduzca un número mayor que 0:")
+
+print("Calcula el factorial")
+print("a) Con recursividad.")
+print("b) Sin recursividad.")
+op=input("Elija una opción: a / b: ")
+
+if op.lower() == "a":
+    num = int(input("Introduzca un número mayor que 0: "))
+    if num < 0:
+        print("Número negativo. Introduzca un número mayor que 0:")
+    else:
+        print("El factorial de ",num," es: ",func_factorial(num))
+elif op.lower() == "b":
+    num = int(input("Introduzca un número mayor que 0: "))
+    if num < 0:
+        print("Número negativo. Introduzca un número mayor que 0:")
+    else:
+        print("El factorial de ",num," es: ",func_factorial2(num))
 else:
-    print("El factorial de ",num," es: ",func_factorial2(num))
+    print("La opción no es correcta. Elija a o b")
