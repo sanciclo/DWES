37b0721859ad9e561010c4e404cbedac3a22c6a7
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-19 13:46:55 +0200

    Ejercicio 1 python

---
+++ sprint2python/concurso.py
@@ -0,0 +1,9 @@
+print("¿Qué río está en la frontera entre Galicia y Portugal?")
+print("a) Duero")
+print("b) Miño")
+print("c) Tajo")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="Miño" or respuesta.lower()=="b":
+    print("Respuesta acertada")
+else:
+    print("Has fallado")
