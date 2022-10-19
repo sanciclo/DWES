77e43c7fb84a04bf9ba0c9f19d9c9d26b4a25e37
Sandra Peteiro <91593594+sanciclo@users.noreply.github.com>
2022-10-19 14:05:14 +0200

    Ejercicio 2 python

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,3 +1,4 @@
+puntos=0
 print("¿Qué río está en la frontera entre Galicia y Portugal?")
 print("a) Duero")
 print("b) Miño")
@@ -5,5 +6,30 @@ print("c) Tajo")
 respuesta=input("Tu respuesta es (a/b/c): ")
 if respuesta=="Miño" or respuesta.lower()=="b":
     print("Respuesta acertada")
+    puntos = puntos + 10
 else:
     print("Has fallado")
+    puntos = puntos - 5
+print("¿Dónde se encuentra el Museo del Prado?")
+print("a) Madrid")
+print("b) Barcelona")
+print("c) Sevilla")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="Madrid" or respuesta.lower()=="a":
+    print("Respuesta acertada")
+    puntos = puntos + 10
+else:
+    print("Has fallado")
+    puntos = puntos - 5
+print("¿Como es la carga del protón")
+print("a) Negativa")
+print("b) Neutra")
+print("c) Positiva")
+respuesta=input("Tu respuesta es (a/b/c): ")
+if respuesta=="Positiva" or respuesta.lower()=="c":
+    print("Respuesta acertada")
+    puntos = puntos + 10
+else:
+    print("Has fallado")
+    puntos = puntos - 5
+print("Puntos totales: ",puntos)
