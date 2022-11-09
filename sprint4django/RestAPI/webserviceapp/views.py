7a2efe7b03f00e2aa6f256a8cac37e6365b652de
sandra <speteiror@fpcoruna.afundacion.org>
2022-11-09 13:56:53 +0100

    Hasta punto 4 terminado

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def pagina_de_prueba(request):
+	return HttpResponse("<h1>Hola caracola</h1>");
 
 # Create your views here.
